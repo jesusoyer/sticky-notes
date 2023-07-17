8d0ba610ae4e5307cd97be333c9254f8082a62cd
jesusoyer <oyervidesjesus017@gmail.com>
2023-07-17 12:00:39 -0500

    new changes

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import  "./index.css";
+
+const App: React.FC = () => {
+  return (
+  <div className="font-montserrat text-3xl">
+    test
+  </div>
+  );
+}
+
+export default App;
